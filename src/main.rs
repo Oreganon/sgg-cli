b24d467bbd1a44836b79cb1d3348c0797330d224
Oreganon <oreganon@protonmail.com>
2023-03-04 10:53:47 +0100

    remove \n from cookie

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use wsgg::Connection;
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() >= 2);
-    let message = args.iter().skip(1).map(|x| x.as_str()).collect::<Vec<&str>>().join(" ");
-    let conn = Connection::new(include_str!("../cookie")).unwrap();
+    let message = args
+        .iter()
+        .skip(1)
+        .map(|x| x.as_str())
+        .collect::<Vec<&str>>()
+        .join(" ");
+    let conn = Connection::new(&include_str!("../cookie").replace("\n", "")).unwrap();
     conn.send(&message);
 }
