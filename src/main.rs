2a9326d319e86290c6acf95ec789d0d76a41103e
Oreganon <oreganon@protonmail.com>
2023-03-04 10:49:44 +0100

    meme

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+use wsgg::Connection;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() >= 2);
+    let message = args.iter().skip(1).map(|x| x.as_str()).collect::<Vec<&str>>().join(" ");
+    let conn = Connection::new(include_str!("../cookie")).unwrap();
+    conn.send(&message);
+}
